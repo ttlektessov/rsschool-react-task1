1dfa4b21b1614b41b752f41d665469bc26c8e3bb
unknown <ttlektessov@gmail.com>
2025-07-10 10:11:44 +0500

    feat: add topcontrols and results components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import TopControls from './components/TopControls';
+import Results from './components/Results';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col w-full h-full bg-gray-800">
+      <TopControls />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
